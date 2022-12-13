e0de1cf43ab03f9a3d045a21f0bee5a25912a43d
shabeeb <shabeeb@shabeebs-Mac-mini.local>
2022-12-13 19:32:29 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React, { useRef } from 'react';
+import { render } from 'react-dom';
+
+import EmailEditor from 'react-email-editor';
+
+const App = (props) => {
+  const emailEditorRef = useRef(null);
+
+  const exportHtml = () => {
+    emailEditorRef.current.editor.exportHtml((data) => {
+      const { design, html } = data;
+      console.log('exportHtml', html);
+    });
+  };
+
+  const onLoad = () => {
+    // editor instance is created
+    // you can load your template here;
+    // const templateJson = {};
+    // emailEditorRef.current.editor.loadDesign(templateJson);
+  }
+
+  const onReady = () => {
+    // editor is ready
+    console.log('onReady');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={exportHtml}>Export HTML</button>
+      </div>
+
+      <EmailEditor ref={emailEditorRef} onLoad={onLoad} onReady={onReady} />
     </div>
   );
-}
+};
+
 
 export default App;
